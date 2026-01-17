4d9a4575d2880db9972cc58421ad15c5f5529293
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-01-17 12:00:37 +0530

    static keyword pract

---
+++ PractiseProblems/src/StatticPract2.java
@@ -0,0 +1,23 @@
+
+public class StatticPract2 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+class Vegetable{
+	
+	static void eat() {
+		System.out.println("i like vegetables");
+	}
+	
+}
+
+class Palak extends Vegetable{
+	
+	
+}
+
+
