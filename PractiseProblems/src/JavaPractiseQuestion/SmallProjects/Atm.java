bd362c18d771e5dab34a7d18070fa60f4106e6fe
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-01-21 17:16:54 +0530

    Custom Exception Handling

--- PractiseProblems/src/JavaPractiseQuestion/SmallProjects/Atm.java
+++ PractiseProblems/src/JavaPractiseQuestion/SmallProjects/Atm.java
@@ -2,13 +2,23 @@ package JavaPractiseQuestion.SmallProjects;
 
 import java.util.Scanner;
 
+
+class  InvalidCustomerException extends Exception {
+	
+	public InvalidCustomerException(String msg)
+	{
+		super(msg);
+	}
+	
+}
+
+
 public class Atm {
 
 	public static void main(String[] args) {
 		
 		Bank bank = new Bank();
 		bank.initiate();
-	
 	}
 }
 
@@ -17,12 +27,10 @@ class AtmMachine{
 	private int acc=2020;
 	private int pass=1010;
 	
-	
 	int accountNumber;
 	int password;
 	
-	
-	
+
 	public void UserInput() 
 	{
 		Scanner sc = new Scanner(System.in);
@@ -33,27 +41,52 @@ class AtmMachine{
 	}
 	
 	
-	public void Verify() 
+	public void Verify() throws InvalidCustomerException 
 	{
 		if(acc==accountNumber && pass==password)
 		{
-			System.out.println("U r valid user");
+			System.out.println("U r valid user, Welcome");
 		}
 		else 
 		{
-			System.out.println("not valid user");
+			InvalidCustomerException invalidCustomerException = new InvalidCustomerException("Invalid Credentials! ");
+			System.out.println(invalidCustomerException.getMessage());
+			throw invalidCustomerException; 
 		}
 	}
 }
 
 class Bank {
 	
-	public void initiate() {
-		
+	public void initiate() 
+	{
 		AtmMachine am = new AtmMachine();
+		try 
+		{
 		am.UserInput();
 		am.Verify();
+		}
+		catch(InvalidCustomerException exception)
+		{
+			System.out.println("Try Again! ");
+			try 
+			{
+			am.UserInput();
+			am.Verify();
+			}
+			catch(InvalidCustomerException exception2)
+			{
+				System.out.println("Try Again! ");
+				try 
+				{
+				am.UserInput();
+				am.Verify();
+				}
+				catch(InvalidCustomerException exception3)
+				{
+					System.out.println("cannot be given any more chances");
+				}
+			}
+		}
 	}
-	
-	
 }
