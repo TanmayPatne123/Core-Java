9a5e78727af2bd90c52dd9abfce8a792becb2f02
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-01-22 10:54:55 +0530

    array greater than 20

---
+++ PractiseProblems/src/JavaPractiseQuestion/Array/Find20IsGreater.java
@@ -0,0 +1,18 @@
+package JavaPractiseQuestion.Array;
+
+public class Find20IsGreater {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a[] = {12, 45, 7, 30, 18};
+
+		for(int collect:a)
+		{
+			if(collect>20) 
+			{
+				System.out.print(collect+" ");
+			}
+		}
+	}
+}
