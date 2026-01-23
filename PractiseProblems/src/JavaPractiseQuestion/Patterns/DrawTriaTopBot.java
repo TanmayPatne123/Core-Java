1ab67a90ffadd6330eac8f86e45d4b5b14b15696
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-01-23 11:04:09 +0530

    Draw triangle top bottom

---
+++ PractiseProblems/src/JavaPractiseQuestion/Patterns/DrawTriaTopBot.java
@@ -0,0 +1,29 @@
+package JavaPractiseQuestion.Patterns;
+
+public class DrawTriaTopBot {
+	
+	public static void main(String[] args) {
+		
+		int n=11;
+		
+		
+		for(int i=0;i<n;i++) {
+			
+			for(int j=0;j<n;j++) {
+				
+				if(i+j==(n-1)/2 || j-i==(n-1)/2 || i-j==(n-1)/2 
+						|| j+i==((n-1)/2+n-1)) 
+				{
+					System.out.print("*");
+				}
+				else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
