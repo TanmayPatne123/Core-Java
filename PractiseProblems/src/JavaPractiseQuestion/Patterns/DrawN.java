326edaaed4a26b02474f4e3ecaa862863fd1dfa4
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-01-23 11:35:13 +0530

    Pattern n

--- PractiseProblems/src/JavaPractiseQuestion/Patterns/DrawN.java
+++ PractiseProblems/src/JavaPractiseQuestion/Patterns/DrawN.java
@@ -1,16 +1,17 @@
 package JavaPractiseQuestion.Patterns;
 
-public class DrawZ {
-	
+public class DrawN {
 	
 	public static void main(String[] args) {
 		
 		int n=11;
+		
+		
 		for(int i=0;i<n;i++) 
 		{
-			for(int j=0;j<n;j++) 
+			for(int j=0;j<n;j++)
 			{
-				if(i==0 || i==n-1 || i+j==n-1)
+				if(j==0 || j==n-1 || i==j)
 				{
 					System.out.print("*");
 				}
@@ -21,5 +22,7 @@ public class DrawZ {
 			}
 			System.out.println();
 		}
+		
 	}
+
 }
