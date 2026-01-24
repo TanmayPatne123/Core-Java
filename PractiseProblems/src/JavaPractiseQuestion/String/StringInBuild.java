7fbc3c6f80e2444d0c71177ab3ec251d0a4324df
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-01-24 18:48:31 +0530

    String in Build Methods

---
+++ PractiseProblems/src/JavaPractiseQuestion/String/StringInBuild.java
@@ -0,0 +1,15 @@
+package JavaPractiseQuestion.String;
+
+public class StringInBuild {
+
+	public static void main(String[] args) {
+
+		String s="RajaRamMohanRoy";
+		
+		System.out.println(s.length());
+		System.out.println(s.toLowerCase());
+		System.out.println(s.toUpperCase());
+		System.out.println(s.substring(4, 12));
+		
+	}
+}
