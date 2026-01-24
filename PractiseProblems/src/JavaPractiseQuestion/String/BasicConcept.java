281d96c960dce6732a24b0ead64f954528ef53c6
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-01-24 18:41:17 +0530

    String addition with String Very Important

--- PractiseProblems/src/JavaPractiseQuestion/String/BasicConcept.java
+++ PractiseProblems/src/JavaPractiseQuestion/String/BasicConcept.java
@@ -5,16 +5,24 @@ public class BasicConcept {
 	
 	public static void main(String[] args) {
 		
-		String s1="Tanmay";
-		String s2 ="Tanmay";
-		System.out.println(s1==s2); // true compare refrence
-		System.out.println(s1.equals(s2));
+//		String s1="Tanmay";
+//		String s2 ="Tanmay";
+//		System.out.println(s1==s2); // true compare refrence
+//		System.out.println(s1.equals(s2));
+//		
+//		System.out.println("-------------------------------");
+//		
+//		String string1 = new String("Kunal");
+//		String string2 = new String("Kunal");
+//		System.out.println(string1==string2); // false
+//		System.out.println(string1.equals(string2)); 
 		
-		System.out.println("-------------------------------");
+		System.out.println("----------------------");
+	
+		String a="mohit";
+		String b = new String("mohit");
+		System.out.println(a.equals(b));
+		System.out.println(a==b);
 		
-		String string1 = new String("Kunal");
-		String string2 = new String("Kunal");
-		System.out.println(string1==string2); // false
-		System.out.println(string1.equals(string2)); 
 	}
 }
---
+++ PractiseProblems/src/JavaPractiseQuestion/String/String2.java
@@ -0,0 +1,22 @@
+package JavaPractiseQuestion.String;
+
+public class String2 {
+	
+	public static void main(String[] args) {
+		
+		String s1="tanmay";
+		System.out.println(s1);
+		
+		System.out.println("---------------------");
+		String concat = s1.concat(" patne");
+		System.out.println(concat);
+		
+		
+		String s="Spring Boot"+6262+72727;
+		System.out.println(s);
+		
+		
+		String n=+10+20+" Hello world";
+		System.out.println(n);
+	}
+}
