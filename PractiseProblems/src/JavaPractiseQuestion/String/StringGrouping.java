0b1bb98bad81e0bdcd4ef12f1f508a3d326769a0
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-01-25 12:29:26 +0530

    String grouping

---
+++ PractiseProblems/src/JavaPractiseQuestion/String/StringGrouping.java
@@ -0,0 +1,23 @@
+package JavaPractiseQuestion.String;
+
+public class StringGrouping {
+	
+	
+	public static void main(String[] args) {
+		
+		String s = "abcdefghij";
+		
+		int size=2;
+		
+		
+		
+		for(int i=0;i<s.length();i=i+size) 
+		{
+			System.out.print(s.substring(i, i+size));
+			if(i+size<s.length())
+			{
+				System.out.print(" | ");
+			}
+		}
+	}
+}
