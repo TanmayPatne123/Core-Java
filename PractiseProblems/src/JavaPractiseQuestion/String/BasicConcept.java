7d93240421f45e682f2f84dbffe776a7b24ec8a5
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-01-28 10:00:45 +0530

    learning String constant pool

--- PractiseProblems/src/JavaPractiseQuestion/String/BasicConcept.java
+++ PractiseProblems/src/JavaPractiseQuestion/String/BasicConcept.java
@@ -17,12 +17,34 @@ public class BasicConcept {
 //		System.out.println(string1==string2); // false
 //		System.out.println(string1.equals(string2)); 
 		
-		System.out.println("----------------------");
-	
-		String a="mohit";
-		String b = new String("mohit");
-		System.out.println(a.equals(b));
-		System.out.println(a==b);
+//		System.out.println("----------------------");
+//	
+//		String a="mohit";
+//		String b = new String("mohit");
+//		System.out.println(a.equals(b));
+//		System.out.println(a==b);
 		
+		String s1 = new String("lnv");
+		String s2 = new String("lnv");
+		
+		System.out.println(s1==s2); //  memory allocation dekhata hain
+		System.out.println(s1.equals(s2)); // string ke andar ki value check krta hain
+		
+		System.out.println("--------------------------------");
+		
+		String v1="dog";
+		String v2="dog";
+		
+		System.out.println(v1==v2); //isme memory same ho jati hain value same hain override hoti hain
+		System.out.println(v1.equals(v2)); // string ke andar ki value check krta hain
+
+		System.out.println("--------------------------------");
+
+		
+		String b1="kunal";
+		
+		String string = new String("kunal");
+		System.out.println(b1==string);
+		System.out.println(b1.equals(string));
 	}
 }
