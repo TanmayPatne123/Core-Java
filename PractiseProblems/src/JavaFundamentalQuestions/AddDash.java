26176af6927204e1afc1681d808a2ef40c1a47ce
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-02-08 15:27:06 +0530

    just revising java fundamental questions

---
+++ PractiseProblems/src/JavaFundamentalQuestions/AddDash.java
@@ -0,0 +1,21 @@
+package JavaFundamentalQuestions;
+
+public class AddDash {
+
+	public static void main(String[] args) {
+
+		 String s = "abcdefgh";
+	        int size = 3;
+
+	        for (int i = 0; i < s.length(); i = i + size) 
+	        {
+	            if (i + size < s.length())
+	            {
+	                System.out.print(s.substring(i, i + size) + " | ");
+	            } else 
+	            {
+	                System.out.print(s.substring(i));
+	            }
+	        }
+	    }
+	}
