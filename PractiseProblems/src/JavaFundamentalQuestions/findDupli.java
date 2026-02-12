5fe6033797d123e096aab32858f822f3041d9d66
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-02-12 18:04:14 +0530

    printing it one time

--- PractiseProblems/src/JavaFundamentalQuestions/findDupli.java
+++ PractiseProblems/src/JavaFundamentalQuestions/findDupli.java
@@ -4,14 +4,17 @@ public class findDupli {
 	
 	public static void main(String[] args) {
 		
-		int [] arr= {22,12,12,44,2,6,6};
-		
+		int [] arr= {0,12,12,44,2,0,6};
+		int found=0;
 		for(int i=0;i<arr.length;i++)
 		{
+			if(found==arr[i])
+				continue;
 			for(int j=0;j<arr.length;j++) 
 			{
 				if(arr[i]==arr[j] && i!=j) 
 				{
+					found=arr[i];
 					System.out.println(arr[i]);
 				}
 			}
