dbb062e28eb24d9fd043460ee2a249f6ee42380a
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-02-14 22:59:07 +0530

    completing name for my friend sushant thanks u

--- PractiseProblems/src/JavaPractiseQuestion/names/DrawSushant.java
+++ PractiseProblems/src/JavaPractiseQuestion/names/DrawSushant.java
@@ -75,6 +75,17 @@ public class DrawSushant {
 					System.out.print(" ");
 				}
 			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if(i==0 || j==(n-1)/2)
+				{
+					System.out.print("*");
+				}else 
+				{
+					System.out.print(" ");
+				}
+			}
 			System.out.println();
 		}
 		
