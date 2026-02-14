a24eb9c7cb063b465530481b23acc95adcc62ee1
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-02-14 22:55:57 +0530

    added a in star pattern

--- PractiseProblems/src/JavaPractiseQuestion/names/DrawSushant.java
+++ PractiseProblems/src/JavaPractiseQuestion/names/DrawSushant.java
@@ -53,7 +53,17 @@ public class DrawSushant {
 					System.out.print(" ");
 				}
 			}
-			
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if(j==0 || i==0 || j==n-1 || i==(n-1)/2)
+				{
+					System.out.print("*");
+				}else 
+				{
+					System.out.print(" ");
+				}
+			}
 			System.out.println();
 		}
 		
