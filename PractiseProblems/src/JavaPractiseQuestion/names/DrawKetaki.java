d27a6743ffe59b98ee950fb7ea9476b6c9671daf
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-02-15 17:46:19 +0530

    drawing t for same name

--- PractiseProblems/src/JavaPractiseQuestion/names/DrawKetaki.java
+++ PractiseProblems/src/JavaPractiseQuestion/names/DrawKetaki.java
@@ -30,6 +30,18 @@ public class DrawKetaki {
 				}
 			}
 			
+			System.out.print("   ");
+			for(int j=0;j<n;j++) 
+			{
+				if(i==0 || j==(n-1)/2)
+				{
+					System.out.print("*");
+				}else 
+				{
+					System.out.print(" ");
+				}
+			}
+			
 			System.out.println();
 		}
 	}
