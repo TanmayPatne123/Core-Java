02e117e2d8fa43d733ae14b2b65a184fc7e8336c
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-02-15 17:49:54 +0530

    draw full name that is ketaki

--- PractiseProblems/src/JavaPractiseQuestion/names/DrawKetaki.java
+++ PractiseProblems/src/JavaPractiseQuestion/names/DrawKetaki.java
@@ -52,6 +52,30 @@ public class DrawKetaki {
 					System.out.print(" ");
 				}
 			}
+			System.out.print("   ");
+			
+			for(int j=0;j<n;j++) 
+			{
+				if(j==0  || i+j==(n-1)/2 || i-j==(n-1)/2)
+				{
+					System.out.print("*");
+				}else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" ");
+			
+			for(int j=0;j<n;j++) 
+			{
+				if(i==0 || j==(n-1)/2 || i==n-1)
+				{
+					System.out.print("*");
+				}else 
+				{
+					System.out.print(" ");
+				}
+			}
 			System.out.println();
 		}
 	}
