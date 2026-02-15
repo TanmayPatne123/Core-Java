b65d4ea44939386aa4812bf7781e5cc49ff424e0
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-02-15 17:48:07 +0530

    draw a for same

--- PractiseProblems/src/JavaPractiseQuestion/names/DrawKetaki.java
+++ PractiseProblems/src/JavaPractiseQuestion/names/DrawKetaki.java
@@ -41,7 +41,17 @@ public class DrawKetaki {
 					System.out.print(" ");
 				}
 			}
-			
+			System.out.print("   ");
+			for(int j=0;j<n;j++) 
+			{
+				if(j==0 || i==0 || j==n-1 || i==(n-1)/2)
+				{
+					System.out.print("*");
+				}else 
+				{
+					System.out.print(" ");
+				}
+			}
 			System.out.println();
 		}
 	}
