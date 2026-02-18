1e32b12bbd06e925dcdfb3e77c3083baa6853b81
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-02-18 23:12:00 +0530

    printing another one

--- PractiseProblems/src/JavaPractiseQuestion/names/DrawAjinkya.java
+++ PractiseProblems/src/JavaPractiseQuestion/names/DrawAjinkya.java
@@ -29,6 +29,40 @@ public class DrawAjinkya {
 					System.out.print(" ");
 				}
 			}
+			System.out.print("   ");
+			for(int j=0;j<n;j++) 
+			{
+				if(j==(n-1)/2 || i==0 || i==n-1)
+				{
+					System.out.print("*");
+				}else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print("   ");
+			for(int j=0;j<n;j++) 
+			{
+				if(j==0 || j==n-1 || i==j)
+				{
+					System.out.print("*");
+				}else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print("   ");
+			for(int j=0;j<n;j++) 
+			{
+				if(j==0 || i+j==(n-1)/2 || i-j==(n-1)/2)
+				{
+					System.out.print("*");
+				}else 
+				{
+					System.out.print(" ");
+				}
+			}
+			
 			System.out.println();
 		}	
 	}
