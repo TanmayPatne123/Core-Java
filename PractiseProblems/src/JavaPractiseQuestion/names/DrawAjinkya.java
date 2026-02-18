a191089b6602007b999381d7da8b6b5dbe814f0e
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-02-18 23:26:02 +0530

    drawing ajinkya full name with pattern task completed

--- PractiseProblems/src/JavaPractiseQuestion/names/DrawAjinkya.java
+++ PractiseProblems/src/JavaPractiseQuestion/names/DrawAjinkya.java
@@ -62,7 +62,27 @@ public class DrawAjinkya {
 					System.out.print(" ");
 				}
 			}
-			
+			System.out.print("   ");
+			for(int j=0;j<n;j++) 
+			{
+				if(i==j && i<=(n-1)/2 || i+j==n-1)
+				{
+					System.out.print("*");
+				}else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if(i==0 || j==0 || j==n-1 || i==(n-1)/2)
+				{
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
 			System.out.println();
 		}	
 	}
