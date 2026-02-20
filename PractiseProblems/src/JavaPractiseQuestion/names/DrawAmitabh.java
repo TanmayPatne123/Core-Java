f76ddd9e7b9df87a8d3fbbdc20f6cfa4dd263121
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-02-20 23:24:35 +0530

    draw full name of big b

--- PractiseProblems/src/JavaPractiseQuestion/names/DrawAmitabh.java
+++ PractiseProblems/src/JavaPractiseQuestion/names/DrawAmitabh.java
@@ -38,6 +38,49 @@ public class DrawAmitabh {
 					System.out.print(" ");
 				}
 			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if(i==0 || j==(n-1)/2)
+				{
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if(i==0 || j==0 || i==(n-1)/2 || j==n-1)
+				{
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if(i==0 || j==0 || i==n-1 || i==(n-1)/2 || j==n-1 )
+				{
+					System.out.print("*");
+				}else 
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if( j==0 || i==(n-1)/2 || j==n-1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
 			System.out.println();
 		}
 	}
