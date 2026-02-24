d794f86a59f542bf2627acc06d1d2a6084b663d8
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-02-24 23:40:03 +0530

    updating pattern

--- PractiseProblems/src/JavaPractiseQuestion/names/DrawKumar.java
+++ PractiseProblems/src/JavaPractiseQuestion/names/DrawKumar.java
@@ -58,7 +58,7 @@ public class DrawKumar {
 			System.out.print("  ");
 			for(int j=0;j<n;j++) 
 			{
-				if(j==0 || i==0 || j==n-1 && i<=(n-1)/2 || i==(n-1)/2 || i-j==(n-1)/2) 
+				if(j==0 || i==0 && j!=n-1 || j==n-1 && i<(n-1)/2 && i!=0  || i==(n-1)/2 && j!=n-1 || i-j==(n-1)/2) 
 				{
 					System.out.print("*");
 				}
