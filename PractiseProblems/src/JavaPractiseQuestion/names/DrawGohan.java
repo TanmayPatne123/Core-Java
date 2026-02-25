66e5e338eb445b8f6a090fbd4528b472009ed62b
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-02-25 23:27:13 +0530

    practising pattern

--- PractiseProblems/src/JavaPractiseQuestion/names/DrawGohan.java
+++ PractiseProblems/src/JavaPractiseQuestion/names/DrawGohan.java
@@ -1,16 +1,16 @@
 package JavaPractiseQuestion.names;
 
-public class DrawKumar {
+public class DrawGohan {
 	
 	public static void main(String[] args) {
-		
-		int n=9;
+		int n=11;
 		
 		for(int i=0;i<n;i++) 
 		{
 			for(int j=0;j<n;j++) 
 			{
-				if(j==0 || i+j==(n-1)/2 && i<=(n-1)/2 || i-j==(n-1)/2) 
+				if(j==0 || i==0 || i==n-1 || j==n-1 && i>(n-1)/2
+						|| i==(n-1)/2 && j>4)
 				{
 					System.out.print("*");
 				}
@@ -19,10 +19,10 @@ public class DrawKumar {
 					System.out.print(" ");
 				}
 			}
-			System.out.print("  ");
+			System.out.print("   ");
 			for(int j=0;j<n;j++) 
 			{
-				if(j==0 && i!=n-1 || i==n-1 && j!=0 && j!=n-1 || j==n-1 && i!=n-1) 
+				if(i==0 || j==0 || i==n-1 || j==n-1)
 				{
 					System.out.print("*");
 				}
@@ -31,10 +31,10 @@ public class DrawKumar {
 					System.out.print(" ");
 				}
 			}
-			System.out.print("  ");
+			System.out.print("   ");
 			for(int j=0;j<n;j++) 
 			{
-				if(j==0 || j==n-1 || i==j && i<=(n-1)/2 || i+j==n-1 && i<=(n-1)/2) 
+				if( j==0 || j==n-1 || i==(n-1)/2)
 				{
 					System.out.print("*");
 				}
@@ -43,10 +43,10 @@ public class DrawKumar {
 					System.out.print(" ");
 				}
 			}
-			System.out.print("  ");
+			System.out.print("   ");
 			for(int j=0;j<n;j++) 
 			{
-				if(j==0 && i>0 || j==n-1 && i!=0 || i==(n-1)/2 || i==0 && j!=0 && j!=n-1) 
+				if(i==0 || j==0 || j==n-1 || i==(n-1)/2)
 				{
 					System.out.print("*");
 				}
@@ -55,10 +55,10 @@ public class DrawKumar {
 					System.out.print(" ");
 				}
 			}
-			System.out.print("  ");
+			System.out.print("   ");
 			for(int j=0;j<n;j++) 
 			{
-				if(j==0 || i==0 && j!=n-1 || j==n-1 && i<(n-1)/2 && i!=0  || i==(n-1)/2 && j!=n-1 || i-j==(n-1)/2) 
+				if(j==0 || j==n-1 || i==j)
 				{
 					System.out.print("*");
 				}
@@ -71,5 +71,6 @@ public class DrawKumar {
 		}
 		
 	}
+	
 
 }
