0f3c84b0cf44ea218c795dad97a2e4650a58d608
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-03-01 22:32:35 +0530

    write david name by pattern

--- PractiseProblems/src/JavaPractiseQuestion/names/DrawArchie.java
+++ PractiseProblems/src/JavaPractiseQuestion/names/DrawArchie.java
@@ -19,7 +19,7 @@ public class DrawArchie {
 			System.out.print("   ");
 			for(int j=0;j<n;j++) 
 			{
-				if(i==0 || j==0 || j==n-1 && i<=(n-1)/2 ||
+				if(i==0 && j!=0 && j!=n-1  || j==0 && i!=0 || j==n-1 && i<=(n-1)/2 && i!=0 ||
 						i==(n-1)/2 || i-j==(n-1)/2)
 				{
 					System.out.print("*");
