5d03b64a204ee196a3f890999df87dab63064399
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-03-11 20:38:05 +0530

    read n lines

--- PractiseProblems/src/JavaPractiseQuestion/HackerRank/ReadNLines.java
+++ PractiseProblems/src/JavaPractiseQuestion/HackerRank/ReadNLines.java
@@ -7,15 +7,23 @@ public class ReadNLines {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		long inc=0;
-		String s="";
-		while(s!=null)
+//		long inc=0;
+//		String s="";
+//		while(s!=null)
+//		{
+//		s = sc.next();
+//		inc++;
+//		System.out.println(inc+" "+s);
+//		}
+		
+		int incr=1;
+		while(sc.hasNextLine()) 
 		{
-		System.out.println("Enter String u Want");
-		s = sc.next();
-		inc++;
-		System.out.println(inc+" "+s);
+			String line = sc.nextLine();
+			System.out.println(incr +" "+ line);
+			incr++;
 		}
+
 	}
 
 }
