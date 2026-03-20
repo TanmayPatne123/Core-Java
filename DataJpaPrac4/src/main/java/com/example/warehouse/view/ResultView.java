4a1f43060a5355445d44f664aa1ebbf8c89952fa
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-03-20 22:26:50 +0530

    result view

---
+++ DataJpaPrac4/src/main/java/com/example/warehouse/view/ResultView.java
@@ -0,0 +1,11 @@
+package com.example.warehouse.view;
+
+public interface ResultView {
+	
+	
+	public String getName();
+	public String getCity();
+	public Integer getId();
+	public Double getCost();
+
+}
