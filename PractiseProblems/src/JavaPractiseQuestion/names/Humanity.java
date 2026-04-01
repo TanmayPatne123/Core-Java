663866dc225c07f942acb8377c3ddb8115e610f9
TanmayPatne123 <p.patnetanmay@gmail.com>
2026-04-01 13:05:19 +0530

    printing pattern

---
+++ PractiseProblems/src/JavaPractiseQuestion/names/Humanity.java
@@ -0,0 +1,111 @@
+package JavaPractiseQuestion.names;
+
+public class Humanity {
+
+	public static void main(String[] args) {
+
+		int n=9;
+		
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++) 
+			{
+				if(j==0 || j==n-1 || i==(n-1)/2) 
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if(j==0 || j==n-1 || i==(n-1)) 
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if(j==0 || j==n-1 || i==j && i<=(n-1)/2 || i+j==n-1 && i<=(n-1)/2) 
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if(j==0 || j==n-1 || i==(n-1)/2 || i==0) 
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if(j==0 || j==n-1 || i==j) 
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if(i==0 || j==(n-1)/2 || i==n-1) 
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if(i==0 || j==(n-1)/2) 
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print("  ");
+			for(int j=0;j<n;j++) 
+			{
+				if(i==j && i<=(n-1)/2 || i+j==n-1) 
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
